1e0efb766c0b1b806ea49bf030e692f4854df0d7
Eyal Agiv <eyala@fb.com>
2022-02-24 13:13:23 +0200

    Implement auto show note logic

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,27 @@
 import joplin from 'api';
+import { SettingItemType } from 'api/types';
 
 joplin.plugins.register({
 	onStart: async function() {
-		console.info('Hello world. Test plugin started!');
+		await joplin.settings.registerSection('autoShowActiveNoteInSideBarSettingsSection', {
+			label: 'Auto Show Active Plugin',
+		});
+
+		await joplin.settings.registerSettings({
+			'autoShowActiveNoteInSideBar': {
+				value: true,
+				type: SettingItemType.Bool,
+				section: 'autoShowActiveNoteInSideBarSettingsSection',
+				public: true,
+				label: 'Toggle auto show active note in side bar',
+			},
+		});
+
+		await joplin.workspace.onNoteSelectionChange(async () => {
+			const isAutoShowActiveNoteInSideBarEnabled = await joplin.settings.value('autoShowActiveNoteInSideBar');
+			if (isAutoShowActiveNoteInSideBarEnabled) {
+				await joplin.commands.execute('focusElementSideBar')
+			}
+		});
 	},
 });
